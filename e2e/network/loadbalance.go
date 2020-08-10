cb74ec35e30ae9f43ba7fdff8b0ee4520a713741
Duan Jiong <djduanjiong@gmail.com>
2020-08-10 15:48:11 +0800

    add loadbalance test
    Signed-off-by: Duan Jiong <djduanjiong@gmail.com>

--- e2e/network/loadbalance.go
+++ e2e/network/loadbalance.go
@@ -1,84 +1,318 @@
 package network
 
 import (
+	"context"
+	"fmt"
 	"github.com/onsi/ginkgo"
-	"github.com/yunify/qingcloud-cloud-controller-manager/pkg/loadbalance"
+	"github.com/yunify/qingcloud-cloud-controller-manager/pkg/apis"
+	"github.com/yunify/qingcloud-cloud-controller-manager/pkg/executor"
+	"github.com/yunify/qingcloud-cloud-controller-manager/pkg/qingcloud"
+	"github.com/yunify/qingcloud-sdk-go/service"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/wait"
 	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2eservice "k8s.io/kubernetes/test/e2e/framework/service"
-	qcservice "github.com/yunify/qingcloud-sdk-go/service"
+	e2eskipper "k8s.io/kubernetes/test/e2e/framework/skipper"
+	"os"
+	"time"
+)
+
+const (
+	defaultWaitLB = 5 * time.Minute
+	defaultConfig = "/etc/qingcloud/lbconfig.yaml"
 )
 
 var _ = KubesphereDescribe("[QingCloud:LoadBalance]", func() {
 	f := framework.NewDefaultFramework("network")
 
-	var c clientset.Interface
-	var qcClient *QingCloud
-	var err error
-	var ns string
+	klog.SetOutput(ginkgo.GinkgoWriter)
+
+	var (
+		c        clientset.Interface
+		qcClient executor.QingCloudClientInterface
+		qcLB     *qingcloud.QingCloud
+		ns       string
+	)
 
 	ginkgo.BeforeEach(func() {
 		c = f.ClientSet
 		ns = f.Namespace.Name
-		qcClient, err = newQingCloud()
+
+		config, err := os.Open(defaultConfig)
+		defer config.Close()
+		framework.ExpectNoError(err)
+		tmp, err := qingcloud.NewQingCloud(config)
+		framework.ExpectNoError(err)
+		qcLB = tmp.(*qingcloud.QingCloud)
+
+		qcClient = qcLB.Client
+	})
+
+	ginkgo.It("Test the encapsulated interface", func() {
+		e2eskipper.Skipf("create loadbalance")
+
+		_, err := qcClient.GetSecurityGroupByName(executor.DefaultSgName)
+		framework.ExpectNoError(err)
+		ginkgo.By("should be tagged")
+
+		ginkgo.By("allocate eip")
+		originEips, err := qcClient.GetAvaliableEIPs()
+		framework.ExpectNoError(err)
+		allocatedEip, err := qcClient.AllocateEIP(nil)
+		framework.ExpectNoError(err)
+		ginkgo.By("allocated eip should be tagged")
+		nowEips, err := qcClient.GetAvaliableEIPs()
+		framework.ExpectEqual(len(originEips)+1, len(nowEips))
+
+		found := false
+		for _, eip := range nowEips {
+			if *eip.Status.EIPID == *allocatedEip.Status.EIPID {
+				found = true
+			}
+		}
+		framework.ExpectEqual(found, true)
+
+		ginkgo.By("create lb")
+		lb, err := qcClient.CreateLB(&apis.LoadBalancer{
+			Spec: apis.LoadBalancerSpec{
+				VxNetID:          service.String(qcLB.Config.DefaultVxNetForLB),
+				EIPs:             []*string{allocatedEip.Status.EIPID},
+				LoadBalancerName: service.String("test-lb"),
+			},
+		})
+		framework.ExpectNoError(err)
+
+		ginkgo.By("get lb by id or name")
+		tmp, err := qcClient.GetLoadBalancerByName(*lb.Spec.LoadBalancerName)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(*tmp.Status.LoadBalancerID, *lb.Status.LoadBalancerID)
+
+		ginkgo.By("modify lb")
+		update := &apis.LoadBalancer{
+			Spec: apis.LoadBalancerSpec{
+				NodeCount: service.Int(3),
+			},
+			Status: apis.LoadBalancerStatus{
+				LoadBalancerID: lb.Status.LoadBalancerID,
+			},
+		}
+		err = qcClient.ModifyLB(update)
+		framework.ExpectNoError(err)
+
+		ginkgo.By("create listener")
+		listener, err := qcClient.CreateListener([]*apis.LoadBalancerListener{&apis.LoadBalancerListener{
+			Spec: apis.LoadBalancerListenerSpec{
+				LoadBalancerID:   lb.Status.LoadBalancerID,
+				BackendProtocol:  service.String("tcp"),
+				ListenerPort:     service.Int(90),
+				ListenerProtocol: service.String("tcp"),
+			},
+		}})
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(len(listener), 1)
+
+		ginkgo.By("create backend")
+		backend, err := qcClient.CreateBackends([]*apis.LoadBalancerBackend{
+			&apis.LoadBalancerBackend{
+				Spec: apis.LoadBalancerBackendSpec{
+					LoadBalancerListenerID: listener[0].Status.LoadBalancerListenerID,
+					ResourceID:             service.String(qcLB.Config.InstanceIDs[0]),
+					Port:                   service.Int(99),
+				},
+			},
+		})
+
+		ginkgo.By("update lb")
+		err = qcClient.UpdateLB(lb.Status.LoadBalancerID)
+		framework.ExpectNoError(err)
+
+		framework.ExpectNoError(err)
+		ginkgo.By("delete backend")
+		err = qcClient.DeleteBackends(backend)
+		framework.ExpectNoError(err)
+
+		ginkgo.By("delete listener")
+		err = qcClient.DeleteListener([]*string{listener[0].Status.LoadBalancerListenerID})
+		framework.ExpectNoError(err)
+
+		ginkgo.By("delete lb")
+		err = qcClient.DeleteLB(lb.Status.LoadBalancerID)
+		framework.ExpectNoError(err)
+
+		ginkgo.By("free eip")
+		framework.ExpectNoError(qcClient.ReleaseEIP([]*string{allocatedEip.Status.EIPID}))
+	})
+
+	ginkgo.It("test parse annotation", func() {
+		testSvc := &v1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-svc",
+				Namespace: "default-ns",
+				UID:       types.UID("hahahahah-hahah"),
+			},
+			Spec: v1.ServiceSpec{
+				Ports: []v1.ServicePort{{
+					Protocol: v1.ProtocolTCP,
+					Port:     80,
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: 80,
+					},
+					NodePort: 18988,
+				},
+				},
+			},
+		}
+
+		_, err := qingcloud.ParseServiceLBConfig(qcLB.Config.ClusterID, testSvc)
+		framework.ExpectError(err)
+
+		testSvc.Annotations = map[string]string{
+			qingcloud.ServiceAnnotationLoadBalancerNetworkType: qingcloud.NetworkModePublic,
+			qingcloud.ServiceAnnotationLoadBalancerEipSource:   qingcloud.UseAvailableOrAllocateOne,
+		}
+		_, err = qingcloud.ParseServiceLBConfig(qcLB.Config.ClusterID, testSvc)
 		framework.ExpectNoError(err)
 	})
 
-	ginkgo.It("test create loadbalance", func() {
+	ginkgo.It("test qingcloud provider interface", func() {
+		e2eskipper.Skipf("haha")
+
+		testSvc := &v1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-svc",
+				Namespace: "default-ns",
+				UID:       types.UID("hahahahah-hahah"),
+				Annotations: map[string]string{
+					qingcloud.ServiceAnnotationLoadBalancerNetworkType: qingcloud.NetworkModePublic,
+					qingcloud.ServiceAnnotationLoadBalancerEipSource:   qingcloud.UseAvailableOrAllocateOne,
+				},
+			},
+			Spec: v1.ServiceSpec{
+				Ports: []v1.ServicePort{{
+					Protocol: v1.ProtocolTCP,
+					Port:     80,
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: 80,
+					},
+					NodePort: 18988,
+				},
+				},
+			},
+		}
+		lbconfig, err := qingcloud.ParseServiceLBConfig(qcLB.Config.ClusterID, testSvc)
+
+		_, exists, err := qcLB.GetLoadBalancer(nil, "", testSvc)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(exists, false)
+
+		testNode := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: qcLB.Config.InstanceIDs[0],
+			},
+		}
+		lb, err := qcLB.EnsureLoadBalancer(nil, "", testSvc, []*v1.Node{testNode})
+		framework.ExpectNoError(err)
+		_, exists, err = qcLB.GetLoadBalancer(nil, "", testSvc)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(exists, true)
+		ginkgo.By("check attribute on lb")
+		tmp, err := qcClient.GetLoadBalancerByName(lbconfig.LoadBalancerName)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(len(tmp.Status.LoadBalancerListeners), 1)
+		framework.ExpectEqual(*tmp.Status.LoadBalancerListeners[0].Spec.ListenerPort, int(testSvc.Spec.Ports[0].Port))
+		framework.ExpectEqual(lb.Ingress[0].IP, tmp.Status.VIP[0])
+
+		testNode2 := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: qcLB.Config.InstanceIDs[1],
+			},
+		}
+		err = qcLB.UpdateLoadBalancer(nil, "", testSvc, []*v1.Node{testNode2})
+		framework.ExpectNoError(err)
+		tmp2, err := qcClient.GetLoadBalancerByName(lbconfig.LoadBalancerName)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(len(tmp2.Status.LoadBalancerListeners), 1)
+		framework.ExpectEqual(int(*tmp2.Status.LoadBalancerListeners[0].Spec.ListenerPort), int(testSvc.Spec.Ports[0].Port))
+		framework.ExpectEqual(*tmp2.Status.LoadBalancerListeners[0].Status.LoadBalancerListenerID, *tmp.Status.LoadBalancerListeners[0].Status.LoadBalancerListenerID, )
+		ginkgo.By("check backend change")
+
+		err = qcLB.EnsureLoadBalancerDeleted(nil, "", testSvc)
+		framework.ExpectNoError(err)
+		_, exists, err = qcLB.GetLoadBalancer(nil, "", testSvc)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(exists, false)
+	})
+
+	ginkgo.It("test loadbalance service normal lifecycle with e2e", func() {
 		tcpJig := e2eservice.NewTestJig(c, ns, "test-service")
-		_, err := tcpJig.CreateTCPService(nil)
 		tcpJig.Run(nil)
-		framework.ExpectNoError(err)
-		_, err = tcpJig.UpdateService(func(s *v1.Service) {
+		_, err := tcpJig.CreateTCPService(func(s *v1.Service) {
 			s.Spec.Type = v1.ServiceTypeLoadBalancer
 			if s.ObjectMeta.Annotations == nil {
 				s.ObjectMeta.Annotations = map[string]string{}
 			}
-			s.Annotations[loadbalance.ServiceAnnotationLoadBalancerType] = "0"
-			s.Annotations[loadbalance.ServiceAnnotationLoadBalancerEipSource] = string(loadbalance.UseAvailableOrAllocateOne)
+			s.Annotations[qingcloud.ServiceAnnotationLoadBalancerType] = "0"
+			s.Annotations[qingcloud.ServiceAnnotationLoadBalancerNetworkType] = qingcloud.NetworkModeInternal
+			s.Annotations[qingcloud.ServiceAnnotationLoadBalancerVxnetID] = qcLB.Config.DefaultVxNetForLB
 		})
-	})
-
-	ginkgo.It("test reuse loadbalance", func() {
-		ginkgo.By("create eip")
-		outputEIP, err := qcClient.eipService.AllocateEIPs(&qcservice.AllocateEIPsInput{
-			Bandwidth:   qcservice.Int(100),
-			BillingMode: nil,
-			Count:       qcservice.Int(1),
-			EIPName:     nil,
-			NeedICP:     nil,
-		})
+		tmpSvc, err := tcpJig.WaitForLoadBalancer(defaultWaitLB)
 		framework.ExpectNoError(err)
-		defer func() {
-			qcClient.eipService.ReleaseEIPs(&qcservice.ReleaseEIPsInput{EIPs:outputEIP.EIPs})
-		}()
+		framework.ExpectEqual(len(tmpSvc.Status.LoadBalancer.Ingress), 1)
 
-		ginkgo.By("create loadbalance")
-		outputLB, err := qcClient.lbService.CreateLoadBalancer(&qcservice.CreateLoadBalancerInput{
-			EIPs:             outputEIP.EIPs,
-			HTTPHeaderSize:   nil,
-			LoadBalancerName: nil,
-			LoadBalancerType: qcservice.Int(0),
-			NodeCount:        qcservice.Int(1),
-			PrivateIP:        nil,
-			ProjectID:        nil,
-			SecurityGroup:    nil,
-			VxNet:            nil,
-		})
+		ginkgo.By("Get the id of the load balancer you created, and use it for other services.")
+		lbconfig, err := qingcloud.ParseServiceLBConfig(qcLB.Config.ClusterID, tmpSvc)
+		outputLB, _ := qcClient.GetLoadBalancerByName(lbconfig.LoadBalancerName)
 		framework.ExpectNoError(err)
-		defer func() {
-			qcClient.lbService.DeleteLoadBalancers(&qcservice.DeleteLoadBalancersInput{LoadBalancers: []*string{outputLB.LoadBalancerID}})
-		}()
 
-		tcpJig := e2eservice.NewTestJig(c, ns, "test-service")
-		_, err = tcpJig.CreateTCPService(func(s *v1.Service) {
+		tcpJig2 := e2eservice.NewTestJig(c, ns, "test-service2")
+		_, err = tcpJig2.CreateTCPService(func(s *v1.Service) {
 			s.Spec.Type = v1.ServiceTypeLoadBalancer
 			if s.ObjectMeta.Annotations == nil {
 				s.ObjectMeta.Annotations = map[string]string{}
 			}
-			s.Annotations[loadbalance.ServiceAnnotationLoadBalancerPolicy] = "test-loadbalance"
+			s.Annotations[qingcloud.ServiceAnnotationLoadBalancerID] = *outputLB.Status.LoadBalancerID
+			s.Spec.Ports[0].Port = 81
 		})
+		tcpJig2.Run(nil)
+		framework.ExpectNoError(err)
+		tmp2Svc, err := tcpJig2.WaitForLoadBalancer(defaultWaitLB)
+		framework.ExpectNoError(err)
+		framework.ExpectEqual(len(tmp2Svc.Status.LoadBalancer.Ingress), 1)
+		framework.ExpectEqual(tmpSvc.Status.LoadBalancer.Ingress[0].IP, tmp2Svc.Status.LoadBalancer.Ingress[0].IP)
+
+		ginkgo.By("Change service type to NodePort, loadbalance status should be deleted")
+		tcpJig2.UpdateService(func(s *v1.Service) {
+			s.Spec.Type = v1.ServiceTypeNodePort
+		})
+		_, err = waitForSvcNoIngress(c, tmp2Svc, defaultWaitLB)
 		framework.ExpectNoError(err)
 	})
 })
+
+
+func waitForSvcNoIngress(client clientset.Interface, svcMeta *v1.Service, timeout time.Duration) (*v1.Service, error) {
+	var service *v1.Service
+
+	pollFunc := func() (bool, error) {
+		svc, err := client.CoreV1().Services(svcMeta.Namespace).Get(context.TODO(), svcMeta.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		if len(svc.Status.LoadBalancer.Ingress) == 0 {
+			service = svc
+			return true, nil
+		}
+		return false, nil
+	}
+	if err := wait.PollImmediate(framework.Poll, timeout, pollFunc); err != nil {
+		return nil, fmt.Errorf("timed out waiting for service %q to %s", svcMeta.Name, "destroy loadbalance")
+	}
+	return service, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/duanjiong/test-framework
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/docker/libnetwork v0.8.0-dev.2.0.20190925143933-c8a5fca4a652 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/kubesphere/porter v0.1.2-0.20200601012936-a7170d3b845f
 	github.com/onsi/ginkgo v1.11.0
@@ -10,22 +12,24 @@ require (
 	github.com/osrg/gobgp v0.0.0-20200501041838-df76b278eee6
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/viper v1.4.0
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.6.1
 	github.com/yunify/qingcloud-cloud-controller-manager v1.4.4
+	github.com/yunify/qingcloud-sdk-go v2.0.0-alpha.38+incompatible
 	google.golang.org/grpc v1.26.0
-	k8s.io/api v0.18.2
-	k8s.io/apimachinery v0.18.2
+	k8s.io/api v1.17.9
+	k8s.io/apimachinery v0.18.8
 	k8s.io/apiserver v0.18.2
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	k8s.io/component-base v0.18.2
 	k8s.io/klog v1.0.0
-	k8s.io/kubernetes v1.18.0
+	k8s.io/kubernetes v1.18.8
 	k8s.io/utils v0.0.0-20200324210504-a9aa75ae1b89
 	sigs.k8s.io/controller-runtime v0.6.0
 )
 
 //For develop
 replace github.com/kubesphere/porter => ../porter
+replace github.com/yunify/qingcloud-cloud-controller-manager => ../qingcloud-cloud-controller-manager
 
 replace (
 	k8s.io/api => k8s.io/kubernetes/staging/src/k8s.io/api v0.0.0-20200325144952-9e991415386e
